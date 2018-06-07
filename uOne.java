75254c168899909663b3df7575a0b74c9fbb910f
Axcce1erat <40032656+Axcce1erat@users.noreply.github.com>
2018-06-07 14:43:07 +0000

    Create uOne.java

---
+++ uOne.java
@@ -0,0 +1,17 @@
+/*
+U-1
+Rocket cost = $100 Million
+Rocket weight = 10 Tonnes
+Max weight (with cargo) = 18 Tonnes
+Chance of launch explosion = 5% * (cargo carried / cargo limit)
+Chance of landing crash = 1% * (cargo carried / cargo limit)
+ */
+public class uOne extends Rocket {
+
+    private int rocketWeight = 10000;
+    private int allWeight;
+    public uOne(){
+        // Chance of launch explosion
+    }
+
+}
