1bbc277f29003ce79a05ac81b90ae6398a265ff6
Axcce1erat <40032656+Axcce1erat@users.noreply.github.com>
2018-06-07 14:40:07 +0000

    Create Item.java

---
+++ Item.java
@@ -0,0 +1,15 @@
+/*
+Create an Item class that includes a String name and an int weight that will represent an item to be carried by the rockets
+ */
+public class Item {
+    String name;
+    int weight;
+    public String toString() {
+        return name + weight;
+    }
+
+    public Item (String name, int weight){
+        this.name = name;
+        this.weight = weight;
+    }
+}
