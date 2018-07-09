24c6f1600bed6d853e23306e56b76623c4c7d226
Axcce1erat <40032656+Axcce1erat@users.noreply.github.com>
2018-07-09 15:23:06 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,25 @@
+import java.io.FileNotFoundException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws FileNotFoundException {
+        Simulation simulation = new Simulation (); // Erzeugt das Objekt Simulation
+        simulation.loadItems(); // Ruft loadItems Methode auf in simulation
+
+        System.out.println("\nLoading uOne for Phase one:\n"); // \n zur übersicht der Darstellung
+        simulation.loadPhase1ForU1();
+
+        System.out.println("\nLoading uTwo for Phase one:\n"); // \n zur übersicht der Darstellung
+        simulation.loadPhase1ForU2();
+
+        System.out.println("\nLoading uOne for Phase two:\n"); // \n zur übersicht der Darstellung
+        simulation.loadPhase2ForU2();
+
+        System.out.println("\nLoading uTwo for Phase two:\n"); // \n zur übersicht der Darstellung
+        simulation.loadPhase2ForU2();
+
+        simulation.UOneSimulation();
+        simulation.UTwoSimulation();
 
     }
 }
